57124e910c24f4bba73ff428f26993f79fa7e273
bombenten <bombenten@hotmail.com>
2020-11-21 18:00:21 +0700

    create var loop scaner

--- Project/src/project/Project.java
+++ Project/src/project/Project.java
@@ -1,30 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package project;
 
-/**
- *
- * @author Bomber
- */
+import java.util.Scanner;
+
 public class Project {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("102");
-        System.out.println("111");
-        System.out.println("hi dose");
-        System.out.println("bom");
-        System.out.println("I am mac");
-        System.out.println(".0.0.");
-        System.out.println("baby go");
-        System.out.println("sawadde krub");
-        System.out.println("KK boi is good guy");
+        int pp, n, dl;
+        int a, b, c, d, av, bv, cv, dv, max;
+        boolean ra, rb, rc, rd;
+
+        ra = true;
+        rb = true;
+        rc = true;
+        rd = true;
+        av = 10;
+        bv = 20;
+        cv = 20;
+        dv = 30;
+        a = 1;
+        b = 1;
+        c = 1;
+        d = 1;
+
+        for (int day = 1; day < 8; day++) {
+            System.out.println("\n\tDay [" + day + "]");
+            max = av * a + bv * b + cv * c + dv * d;
+            System.out.println("There are " + max + " seats.");
+            System.out.print("Enter people for lent : ");
+            pp = STDIN_SCANNER.nextInt();
+            System.out.print("Enter day for lent : ");
+            dl = STDIN_SCANNER.nextInt();
+            System.out.println("Date of return : " + (dl + day));
+            System.out.print("Car : ");
+            
+            
+            
+        }
     }
-    
+    public final static Scanner STDIN_SCANNER = new Scanner(System.in);
 }
