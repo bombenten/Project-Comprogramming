72d6256679d02595961a9cfa630b34973e1cba2a
hunnymc <69097896+hunnymc@users.noreply.github.com>
2020-11-21 16:14:47 +0700

    Start

--- Project/src/project/Project.java
+++ Project/src/project/Project.java
@@ -1,30 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package project;
 
-/**
- *
- * @author Bomber
- */
 public class Project {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("102");
-        System.out.println("111");
-        System.out.println("hi dose");
-        System.out.println("bom");
-        System.out.println("I am mac");
-        System.out.println(".0.0.");
-        System.out.println("baby go");
-        System.out.println("sawadde krub");
-        System.out.println("KK boi is good guy");
     }
     
 }
