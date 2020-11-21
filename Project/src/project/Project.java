5f1cae625652037eba7a3d04cffee556805cd1d1
bombenten <bombenten@hotmail.com>
2020-11-21 18:25:43 +0700

    Change name variable

--- Project/src/project/Project.java
+++ Project/src/project/Project.java
@@ -5,83 +5,82 @@ import java.util.Scanner;
 public class Project {
 
     public static void main(String[] args) {
-        int pp, n, dl;
-        int a, b, c, d, av, bv, cv, dv, max;
+        int people, n, dayLent;
+        int checkA, checkB, checkC, checkD, carA, carB, carC, carD, max;
         int ra, rb, rc, rd;
 
         ra = 1;
         rb = 1;
         rc = 1;
         rd = 1;
-        av = 10;
-        bv = 20;
-        cv = 20;
-        dv = 30;
-        a = 1;
-        b = 1;
-        c = 1;
-        d = 1;
+        carA = 10;
+        carB = 20;
+        carC = 20;
+        carD = 30;
+        checkA = 1;
+        checkB = 1;
+        checkC = 1;
+        checkD = 1;
 
         for (int day = 1; day < 8; day++) {
             System.out.println("\n\tDay [" + day + "]");
-            max = av * a + bv * b + cv * c + dv * d;
+            max = carA * checkA + carB * checkB + carB * checkC + carD * checkD;
             System.out.println("There are " + max + " seats.");
             System.out.print("Enter people for lent : ");
-            pp = STDIN_SCANNER.nextInt();
+            people = STDIN_SCANNER.nextInt();
             System.out.print("Enter day for lent : ");
-            dl = STDIN_SCANNER.nextInt();
-            System.out.println("Date of return : " + (dl + day));
+            dayLent = STDIN_SCANNER.nextInt();
+            System.out.println("Date of return : " + (dayLent + day));
             System.out.print("Car : ");
-            
-        if(pp>max||pp<=0){
-		System.out.print("0");
-	}
-	else{
-            if(rd<=day && pp>20 && pp!=20&&dl>0){
-                d=d-1;
-                rd = day + dl;
-                System.out.print("4 ");
-                pp = pp - 30 ;
-            }
-                    if(b==1&&pp>10&&dl>0){
-                            b=b-1;
-                            rb = day + dl;
-                            System.out.print("2 ");
-                            pp = pp - 20 ;	
-                    }
-                    if(c==1&&pp>10&&dl>0){
-                            c=c-1;
-                            rc = day + dl;	
-                            System.out.print("3 ");
-                            pp = pp - 20 ;		
-                    }
-                    if(d==1&&pp==20&&dl>0){
-                            d=d-1;
-                            rd = day + dl;
-                            System.out.print("4 ");
-                            pp = pp - 30 ;
-                    }
-                    if(a==1&&pp<=10&&pp!=0&&dl>0){
-                            a=a-1;
-                            ra = day + dl;
-                            System.out.print("1 ");	
-                    }
+
+            if (people > max || people <= 0) {
+                System.out.print("0");
+            } else {
+                if (rd <= day && people > 20 && people != 20 && dayLent > 0) {
+                    checkD = checkD - 1;
+                    rd = day + dayLent;
+                    System.out.print("4 ");
+                    people = people - 30;
+                }
+                if (checkB == 1 && people > 10 && dayLent > 0) {
+                    checkB = checkB - 1;
+                    rb = day + dayLent;
+                    System.out.print("2 ");
+                    people = people - 20;
+                }
+                if (checkC == 1 && people > 10 && dayLent > 0) {
+                    checkC = checkC - 1;
+                    rc = day + dayLent;
+                    System.out.print("3 ");
+                    people = people - 20;
+                }
+                if (checkD == 1 && people == 20 && dayLent > 0) {
+                    checkD = checkD - 1;
+                    rd = day + dayLent;
+                    System.out.print("4 ");
+                    people = people - 30;
+                }
+                if (checkA == 1 && people <= 10 && people != 0 && dayLent > 0) {
+                    checkA = checkA - 1;
+                    ra = day + dayLent;
+                    System.out.print("1 ");
+                }
 
             }
-            if(rd<=day+1&&d==0){
-                    d++;
+            if (rd <= day + 1 && checkD == 0) {
+                checkD++;
             }
-            if(rb<=day+1&&b==0){
-                    b++;
+            if (rb <= day + 1 && checkB == 0) {
+                checkB++;
             }
-            if(rc<=day+1&&c==0){
-                    c++;	
+            if (rc <= day + 1 && checkC == 0) {
+                checkC++;
             }
-            if(ra<=day+1&&a==0){
-                    a++;
+            if (ra <= day + 1 && checkA == 0) {
+                checkA++;
             }
-            System.out.println("\n");	
-    }
+            System.out.println("\n");
+        }
 
     }
     public final static Scanner STDIN_SCANNER = new Scanner(System.in);
