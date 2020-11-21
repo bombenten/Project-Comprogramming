712fcb83b084c906e30937ab7afe5bd276fe2485
hunnymc <69097896+hunnymc@users.noreply.github.com>
2020-11-21 18:39:00 +0700

    updateCar

--- Project/src/RentCar/Car/Car3.java
+++ Project/src/RentCar/Car/Car3.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package RentCar.Car;
 
-/**
- *
- * @author Mook
- */
+
 public class Car3 {
-    
+    int seet = 4 ;
+    int rentdate = 0;
 }
--- Project/src/RentCar/Car/Car4.java
+++ Project/src/RentCar/Car/Car4.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package RentCar.Car;
 
-/**
- *
- * @author Mook
- */
+
 public class Car4 {
-    
+    int seet = 7 ;
+    int rentdate = 0;
 }
